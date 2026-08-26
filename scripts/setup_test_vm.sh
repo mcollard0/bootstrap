f4139c35919e4241978dc16968b75247415b5618
Michael Collard <mcollard@gmail.com>
2026-08-26 11:07:34 -0500

    fix: add CYAN color definition in setup_test_vm.sh

--- scripts/setup_test_vm.sh
+++ scripts/setup_test_vm.sh
@@ -27,6 +27,7 @@ readonly GREEN='\033[0;32m'
 readonly YELLOW='\033[1;33m'
 readonly BLUE='\033[0;34m'
 readonly RED='\033[0;31m'
+readonly CYAN='\033[0;36m'
 readonly BOLD='\033[1m'
 readonly NC='\033[0m'
 
